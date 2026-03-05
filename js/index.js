fe48f5db69e775396b9fa27781237b9a4a253ab4
Wasif Hasan <wasifhasancse@gmail.com>
2026-03-05 13:15:16 +0600

    develop show Word details Modal section

--- js/index.js
+++ js/index.js
@@ -5,6 +5,45 @@ const lessonButtonSection = () => {
     .then((getButtonJson) => showButton(getButtonJson.data));
 };
 
+// get word details modal section
+const getWordDetails = (wordId) => {
+  fetch(`https://openapi.programming-hero.com/api/word/${wordId}`)
+    .then((wordDetails) => wordDetails.json())
+    .then((wordDetailsJson) => showWordDetails(wordDetailsJson.data)
+    );
+};
+
+// show similar Word 
+const getSimilarWord = (similarWords) => {
+  const similarWordContainer = similarWords.map((similarWord) => `<span class="btn btn-soft btn-primary">${similarWord}</span>`);
+  return similarWordContainer.join(" ");
+};
+
+// show word details modal section
+const showWordDetails = (wordDetailsData) => {
+  const modalDetailsContainer = document.getElementById("modal-details");
+  modalDetailsContainer.innerHTML = " ";
+  const modalDetails = document.createElement("div");
+  modalDetails.className = "space-y-5";
+  modalDetails.innerHTML = `
+  <h2 class="text-2xl font-bold">${wordDetailsData.word} (<i class="fa-solid fa-microphone-lines"></i> : <span class="font-bangla">${wordDetailsData.pronunciation}</span> )</h2>
+        <div>
+          <h3 class="font-semibold text-lg">Meaning</h3>
+          <p class="font-medium pt-1 font-bangla text-lg">${wordDetailsData.meaning}</p>
+        </div>
+        <div>
+          <h3 class="font-semibold text-lg">Example</h3>
+          <p class="font-medium pt-1">${wordDetailsData.sentence}</p>
+        </div>
+        <div>
+          <h3 class="font-semibold text-lg font-bangla">সমার্থক শব্দ গুলো</h3>
+          <div id="similar-word">${getSimilarWord(wordDetailsData.synonyms)}</div>
+        </div>
+  `;
+  modalDetailsContainer.append(modalDetails);
+  wordDetailsModal.showModal();
+};
+
 // remove Lesson Active Button
 const removeLessonActiveButton = () => {
   const lessonAllButton = document.querySelectorAll(".lesson-button");
@@ -38,7 +77,7 @@ const showWordData = (getWordData) => {
     emptyMessage.classList =
       "col-span-full bg-base-300 py-20 w-11/12 mx-auto rounded-xl flex flex-col items-center justify-center text-center gap-5";
     emptyMessage.innerHTML = `
-    
+
         <img class="mx-auto" src="./assets/alert-error.png" alt="alert-error">
         <p class="font-bangla text-md md:text-lg text-gray-600">
           এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।
@@ -61,7 +100,7 @@ const showWordData = (getWordData) => {
           <p class="font-bangla font-semibold text-2xl text-gray-600">${wordData.meaning ? wordData.meaning : "Meaning পাওয়া যায় নি"} / ${wordData.pronunciation ? wordData.pronunciation : "Pronunciation পাওয়া যায় নি"}</p>
         </div>
         <div class="flex items-center justify-between">
-          <button onclick="wordDetailsModal.showModal()" class="btn btn-soft btn-primary text-xl"><i class="fa-solid fa-circle-info"></i></button>
+          <button onclick="getWordDetails(${wordData.id})" class="btn btn-soft btn-primary text-xl"><i class="fa-solid fa-circle-info"></i></button>
           <button class="btn btn-soft btn-primary text-xl"><i class="fa-solid fa-volume-high"></i></button>
         </div>
     `;
